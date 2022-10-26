5e4fe528df7db6f8542d4fd0494c20c6dacf0bd0
NeyamulOmy <neyamul.omy@gmail.com>
2022-10-27 01:36:39 +0600

    created different apis for sidebar and courses section

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const port = process.env.PORT || 5000;
 
+app.use(cors())
+const courses = require('./data/fakeData.json');
 app.get('/', (req, res) => {
     res.send('Api running')
 });
+
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id)
+    const selectedCourse = courses.find(n => n.id == id);
+    res.send(selectedCourse);
+
+})
 
 app.listen(port, () => {
     console.log('SkillX server running on port', port);
